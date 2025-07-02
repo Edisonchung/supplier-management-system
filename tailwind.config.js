052d81fac13301d585908a7224a68b7d56783260
Edisonchung <Edisonchung@users.noreply.github.com>
2025-07-02 23:01:31 +0800

    Update tailwind.config.js
    tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,15 @@
-/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        'pulse': 'pulse 2s cubic-bezier(0.4, 0, 0.6, 1) infinite',
+      }
+    },
   },
   plugins: [],
 }
