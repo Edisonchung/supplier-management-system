3f12870c5d4cacd528db9c1b23faa0a8fc923739
Edisonchung <Edisonchung@users.noreply.github.com>
2025-07-14 11:49:16 +0800

    Update PIModal.jsx

--- src/components/procurement/PIModal.jsx
+++ src/components/procurement/PIModal.jsx
@@ -322,8 +322,8 @@ const PIModal = ({ proformaInvoice, suppliers, products, onSave, onClose, addSup
     const total = subtotal - discount + shipping + tax;
     
    // setTotalAmount(total);
-   // setFormData(prev => ({ ...prev, totalAmount: total }));
-  // }, [selectedProducts, formData.subtotal, formData.discount, formData.shipping, formData.tax]);
+    setFormData(prev => ({ ...prev, totalAmount: total }));
+   }, [selectedProducts, formData.subtotal, formData.discount, formData.shipping, formData.tax]);
 
   const generatePINumber = () => {
     const date = new Date();
