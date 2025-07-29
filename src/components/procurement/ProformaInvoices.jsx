2e8ee5e8d707b9aa34e96a752abbd92e07fa31aa
Edisonchung <Edisonchung@users.noreply.github.com>
2025-07-29 22:19:32 +0800

    Update ProformaInvoices.jsx

--- src/components/procurement/ProformaInvoices.jsx
+++ src/components/procurement/ProformaInvoices.jsx
@@ -1765,7 +1765,9 @@ const ProformaInvoices = ({ showNotification }) => {
             
             <div className="p-6 max-h-[calc(90vh-120px)] overflow-y-auto">
               <DocumentViewer
-                documentId={documentsModal.pi?.id}
+                //documentId={documentsModal.pi?.id}
+                  documentId={documentsModal.pi?.documentId || documentsModal.pi?.id}
+
                 documentType="pi"
                 documentNumber={documentsModal.pi?.piNumber}
                 allowDelete={true}
