d1f279078e417d31508cf8a82fa9d16ef8f4a7d0
Edisonchung <Edisonchung@users.noreply.github.com>
2025-08-18 21:34:32 +0800

    Update index.js

--- src/services/firestore/index.js
+++ src/services/firestore/index.js
@@ -1,4 +1,7 @@
 export { suppliersService } from './suppliers.service';
 export { productsService } from './products.service';
 export { purchaseOrdersService } from './purchaseOrders.service';
+export { analyticsService } from './analytics.service';        
+export { factoriesService } from './factories.service';      
+export { quotesService } from './quotes.service';         
 export { BaseFirestoreService } from './baseService';
